c9f027039f003d70b8adbaad9953cf81029255e7
jpryor812 <85300205+jpryor812@users.noreply.github.com>
2024-09-23 22:08:27 -0400

    Update index.tsx

--- pages/answer-questions-Q1/index.tsx
+++ pages/answer-questions-Q1/index.tsx
@@ -1,47 +1,55 @@
-import type { NextPage } from "next";
-import styles from "./Q1.module.css";
+import type { NextPage } from 'next';
+import styles from './Q1.module.css';
 
-const AnswerQuestionsQ: NextPage = () => {
+const AnswerQuestionsQ1: NextPage = () => {
   return (
-    <div className={styles.answerQuestionsQ1}>
-      <div className={styles.answerQuestionsQ1Child} />
+    <div>
+      {/* Header Section */}
       <div className={styles.nufriendsHeaderFrame}>
         <div className={styles.nufriends}>nufriends</div>
-        <img
-          className={styles.robotHeaderImageIcon}
-          alt=""
-          src="/robotheaderimage.svg"
-        />
+        <img className={styles.robotHeaderImageIcon} alt="Robot Header" src="robot-header-image.png" />
       </div>
-      <div className={styles.answerQuestionsQ1Item} />
-      <div className={styles.textBoxWrapper}>
+      
+      {/* Question Textbox */}
+      <div className={styles.textBoxParent}>
         <div className={styles.textBox}>
           <div className={styles.textBoxChild} />
-          <div className={styles.whereAreYou}>
-            Where are you from? Did you enjoy growing up there? Why or why not?
-          </div>
+          <div className={styles.whereAreYou}>Where are you from? Did you enjoy growing up there? Why or why not?</div>
         </div>
       </div>
-      <div className={styles.yuBigSpeakWrapper}>
-        <img className={styles.yuBigSpeak} alt="" src="/yu-big-speak.svg" />
+
+      {/* Yu Big Speak Section */}
+      <div className={styles.groupYuBigSpeakParent}>
+        <img className={styles.groupYuBigSpeakIcon} alt="Yu Big Speak" src="Group Yu-big-speak.svg" />
+        <img className={styles.rectangleIcon} alt="Rectangle 49" src="Rectangle 49.svg" />
+        <img className={styles.rectangleIcon} alt="Rectangle 50" src="Rectangle 50.svg" />
+        <div className={styles.yuBig}>
+          <img className={styles.yuBigIcon} alt="Yu Big" src="Yu-big.svg" />
+        </div>
       </div>
-      <div className={styles.question112Wrapper}>
+
+      {/* Question Progress */}
+      <div className={styles.question112Parent}>
         <div className={styles.question112}>Question 1/12</div>
       </div>
-      <div className={styles.answerQuestionsQ1Inner}>
+
+      {/* Textarea and Mic Section */}
+      <div className={styles.groupParent}>
         <div className={styles.groupWrapper}>
           <div className={styles.rectangleParent}>
             <div className={styles.groupChild} />
             <div className={styles.textarea}>{`<textarea>`}</div>
-            <img className={styles.micIcon} alt="" src="/mic.svg" />
+            <img className={styles.micIcon} alt="Mic Icon" src="mic.svg" />
             <div className={styles.groupItem} />
-            <img className={styles.groupInner} alt="" src="/arrow-1.svg" />
+            <img className={styles.groupInner} alt="Arrow" src="Arrow 1.svg" />
           </div>
         </div>
       </div>
+
+      {/* Spacing Div */}
       <div className={styles.frameDiv} />
     </div>
   );
 };
 
-export default AnswerQuestionsQ;
+export default AnswerQuestionsQ1;
