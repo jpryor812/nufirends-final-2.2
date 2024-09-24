f7863a42cddc935a555529e44bfd02cb2bb50a34
jpryor812 <85300205+jpryor812@users.noreply.github.com>
2024-09-24 07:16:21 -0400

    Update index.tsx

--- pages/answer-questions-Q1/index.tsx
+++ pages/answer-questions-Q1/index.tsx
@@ -5,37 +5,43 @@ const AnswerQuestionsQ1: NextPage = () => {
   return (
     <div className={styles.container}>
       {/* Header Section */}
-      <div className={styles.nufriendsHeaderFrame}>
-        <div className={styles.nufriends}>nufriends</div>
-        <img className={styles.robotHeaderImageIcon} alt="Robot Header" src="robot-header-image.png" />
+      <div className={styles.frameDiv}>
+        <div className={styles.nufriendsHeaderFrame}>
+          <div className={styles.nufriends}>nufriends</div>
+          <img className={styles.robotHeaderImageIcon} alt="Robot Header" src="robot-header-image.png" />
+        </div>
       </div>
       
       {/* Question Textbox */}
-      <div className={styles.textBoxWrapper}>
+      <div className={styles.textBoxParent}>
         <div className={styles.textBox}>
-          <div className={styles.textBoxChild} />
-          <div className={styles.whereAreYou}>Where are you from? Did you enjoy growing up there? Why or why not?</div>
+          <div className={styles.textBoxChild}>
+            <div className={styles.whereAreYou}>Where are you from? Did you enjoy growing up there? Why or why not?</div>
+          </div>
         </div>
       </div>
 
       {/* Yu Big Speak Section */}
-      <div className={styles.yuBigSection}>
-        <img className={styles.yuBigIcon} alt="Yu Big" src="Yu-big.svg" />
-        <img className={styles.ellipseIcon} alt="Ellipse 16" src="Ellipse 16.svg" />
+      <div className={styles.groupYuBigSpeakParent}>
+        <img className={styles.groupYuBigSpeakIcon} alt="Yu Big" src="Yu-big.svg" />
         <img className={styles.rectangleIcon} alt="Rectangle 49" src="Rectangle 49.svg" />
         <img className={styles.rectangleIcon} alt="Rectangle 50" src="Rectangle 50.svg" />
       </div>
 
       {/* Question Progress */}
-      <div className={styles.questionProgress}>
+      <div className={styles.question112Parent}>
         <div className={styles.question112}>Question 1/12</div>
       </div>
 
       {/* Textarea and Mic Section */}
-      <div className={styles.textareaWrapper}>
-        <div className={styles.textareaBox}>
-          <textarea className={styles.textarea} placeholder="Type your answer here..." />
-          <img className={styles.micIcon} alt="Mic Icon" src="mic.svg" />
+      <div className={styles.groupParent}>
+        <div className={styles.groupWrapper}>
+          <div className={styles.groupChild}>
+            <textarea className={styles.textarea} placeholder="Type your answer here..." />
+            <div className={styles.groupItem}>
+              <img className={styles.groupInner} alt="Mic Icon" src="mic.svg" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
